a3104c1587512b6ad04ec33713773e558eeac5e2
최종혁 <cardview0806@gmail.com>
2022-01-27 15:28:39 +0900

    11

--- bitbooks01.sql
+++ bitbooks01.sql
@@ -1,24 +1,23 @@
 create table code_class(
-	class_code char(3)NOT NULL primary key,
+	class_code char(3) primary key,
 	class_name varchar(30)NOT NULL,
 	use_yn char(1)NOT NULL DEFAULT 'Y',
 	reg_date TIMESTAMP NOT NULL DEFAULT now(),
 	upd_date TIMESTAMP DEFAULT now()
 );
-
+drop table code_class;
 describe code_class;
 
 
 create table code_detail(
-	code_value varchar(3) NOT NULL,
+	code_value varchar(3) primary key,
     class_code char(3)NOT NULL,
     class_name varchar(30)NOT NULL,
     sort_seq int(10) NOT NULL,
     use_yn char(1) NOT NULL DEFAULT 'Y',
     reg_date TIMESTAMP NOT NULL DEFAULT now(),
     upd_date TIMESTAMP DEFAULT now(),
-    primary key(class_code)
-    
+    CONSTRAINT class_code FOREIGN KEY(class_code) REFERENCES code_class(class_code)
 );
 
 describe code_detail;
